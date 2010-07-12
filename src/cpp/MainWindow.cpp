736aea69a261da3cf87c8148f80dd50b65f62022
Alexey Pavlyukov <lexaficus@yandex.ru>
2010-07-12 18:33:24 +0400

    added feature to change live image size

--- src/cpp/MainWindow.cpp
+++ src/cpp/MainWindow.cpp
@@ -3,13 +3,125 @@
 #include <QtNetwork>
 #include <QtDebug>
  
-MainWindow::MainWindow(): http(new QHttp), buffer(new QBuffer),
+MainWindow::MainWindow(): isRunning(false), http(new QHttp), buffer(new QBuffer),
 			  imgReqId(-1), pixItem(0), view(0) {
+
+  ui.setupUi(this);
+
+  readSettingTimer = new QTimer(this);
+  connect(readSettingTimer, SIGNAL(timeout()), this, SLOT(readSettingTimeout()));
+
+  netManager = new QNetworkAccessManager(this);
+
   view = new QGraphicsView(new QGraphicsScene, this);
   setCentralWidget(view);
   http->setHost("192.168.0.9", 8081);
   connect(http, SIGNAL(requestFinished(int, bool)), this, SLOT(requestFinished(int, bool)));
-  getImage();
+
+  init();
+}
+
+void MainWindow::init() {
+
+  setDcm(1);
+
+  QVariantMap settings = readSettings();
+  qDebug() << settings;
+
+  int iWidth = settings["actual_width"].toInt();
+  int iHeight = settings["actual_height"].toInt();
+
+  for(int i = 1; iHeight / i > 319; ++i) {
+    int h = iHeight / i;
+    int w = iWidth / i;
+    ui.cbLiveImageSize->addItem(QString::number(w) + "x" + QString::number(h), i);
+  }
+  isRunning = true;
+  readSettingTimer->start(500);
+}
+
+MainWindow::~MainWindow() {
+  readSettingTimer->stop();
+  delete readSettingTimer;
+  delete http;
+  delete buffer;
+}
+
+QVariantMap MainWindow::parseSettings(const QByteArray& data) {
+  QVariantMap result;
+  QList<QByteArray> values = data.split('\n');
+  for(QList<QByteArray>::iterator i = values.begin(); i != values.end(); ++i) {
+    QList<QByteArray> key_v = i->split('=');
+    if(key_v.size() != 2) continue;
+    result[key_v[0]] = key_v[1];//.toInt();
+  }
+  return result;
+}
+
+QVariantMap MainWindow::readSettings() {
+
+  QNetworkReply* reply = netManager->get(QNetworkRequest(QUrl("http://192.168.0.9/elcap.php?status=1")));
+  while(!reply->isFinished() && reply->error() == QNetworkReply::NoError) QCoreApplication::processEvents();
+  QVariantMap settings = parseSettings(reply->readAll());
+  delete reply;
+  return settings;
+}
+
+void MainWindow::on_cbLiveImageSize_currentIndexChanged(int index) {
+  if (! isRunning) return;
+  int dcm = ui.cbLiveImageSize->itemData(index).toInt();
+  setDcm(dcm);
+}
+
+void MainWindow::on_pbAcquire_clicked(bool cheked) {
+  if (! isRunning) return;
+  int index = ui.cbLiveImageSize->currentIndex();
+  int dcm = ui.cbLiveImageSize->itemData(index).toInt();
+  isRunning = false;
+  setDcm(1);
+  for(int i=1;true; ++i) {
+    QString fn = QString::number(i) + ".jpg";
+    if(QFile::exists(fn)) continue;
+    QFile file(fn);
+    file.open(QIODevice::WriteOnly);
+    file.write(getImageData());
+
+    break;
+  }
+  setDcm(dcm);
+  isRunning = true;
+  qDebug() << "pbAcquire_clicked";
+}
+
+void MainWindow::setDcm(int dcm) {
+  QString url = "http://192.168.0.9/elcap.php?dcm_hor=" + QString::number(dcm) + "&dcm_vert=" + QString::number(dcm);
+  QNetworkReply* reply = netManager->get(QNetworkRequest(QUrl(url)));
+  while(!reply->isFinished() && reply->error() == QNetworkReply::NoError) QCoreApplication::processEvents();
+  delete reply;
+}
+
+QByteArray MainWindow::getImageData() {
+  QString url = "http://192.168.0.9:8081/last/wait/torp/wait/next/save";
+  QNetworkReply* reply = netManager->get(QNetworkRequest(QUrl(url)));
+  while(!reply->isFinished() && reply->error() == QNetworkReply::NoError) QCoreApplication::processEvents();
+  delete reply;
+
+  url = "http://192.168.0.9:8081/img";
+  reply = netManager->get(QNetworkRequest(QUrl(url)));
+  while(!reply->isFinished()) QCoreApplication::processEvents();
+  QByteArray result = reply->readAll();
+  delete reply;  
+  return result;
+}
+
+void MainWindow::readSettingTimeout() {
+  if(! isRunning) return;
+  if (imgReqId == -1) getImage();
+
+  /*
+  settingsBuffer->reset();
+  readSettingReqId = httpSettings->get("/var/elcap.php?status=1", settingsBuffer);
+  */
 }
 
 void MainWindow::getImage() {
@@ -19,6 +131,9 @@ void MainWindow::getImage() {
 
 void MainWindow::requestFinished(int id, bool error) {
   if(id != imgReqId) return;
+  imgReqId = -1;
+
+  if(! isRunning) return;
 
   QImage image;
   
@@ -29,8 +144,6 @@ void MainWindow::requestFinished(int id, bool error) {
   else
     pixItem->setPixmap(pixmap);
 
-  qDebug() << "done" << id << "; error" << error;
-  qDebug() << "imgSize" << buffer->size();
   getImage();
 }
 
--- src/cpp/MainWindow.hpp
+++ src/cpp/MainWindow.hpp
@@ -5,23 +5,37 @@
 #include <boost/shared_ptr.hpp>
 #include <QBuffer>
 #include <QtNetwork>
+#include "../ui_MainWindow.h"
 
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
 public:
   MainWindow();
+  ~MainWindow();
 protected:
   void closeEvent(QCloseEvent *event);
   void getImage();
+  QByteArray getImageData();
+  void init();
+  QVariantMap parseSettings(const QByteArray& data);
+  QVariantMap readSettings();
+  void setDcm(int dcm);
 private Q_SLOTS:
   void requestFinished(int id, bool error);
+  void readSettingTimeout();
+  void on_cbLiveImageSize_currentIndexChanged(int index);
+  void on_pbAcquire_clicked(bool cheked);
 private:
+  bool isRunning;
   QHttp* http;
   QBuffer* buffer;
   int imgReqId;
   QGraphicsPixmapItem* pixItem;
   QGraphicsView* view;
+  QTimer* readSettingTimer;
+  Ui_MainWindow ui;
+  QNetworkAccessManager* netManager;
 };
 
 #endif
